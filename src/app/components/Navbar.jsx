3dc4f8a2e05b03280efc44267ee7842d42adcb21
IftekarRahmanRuhit <iftekarrahmanruhit@gmail.com>
2025-03-23 00:49:49 +0600

    navigation

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
@@ -5,9 +6,17 @@ export default function Navbar() {
     <div className='w-11/12 mx-auto'>
       <nav className='bg-amber-100 text-black' >
         <ul className='flex justify-center items-center text-center gap-3 mt-3 ' >
+
+        <Link href="/">
         <li>Home</li>
+        </Link>
+        <Link href="/services">
         <li>Services</li>
+        </Link>
+        <Link href="/contact">
         <li>Contact</li>
+        </Link>
+        
         </ul>
       </nav>
     </div>
