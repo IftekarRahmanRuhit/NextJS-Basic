ddb2ef181aa56c194813b7a71295f7ff8a2a4358
IftekarRahmanRuhit <iftekarrahmanruhit@gmail.com>
2025-04-09 14:59:29 +0600

    navigation using router hook

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,32 @@
+"use client"
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function page() {
+// using router hooks for navigation 
+  const router = useRouter()
+  const isLoggedIn = true;
+
+  const handleNavigation = ()=> {
+    if(isLoggedIn){
+      router.push("/contact/address")
+    }
+    else{
+      router.push("/")
+    }
+  }
+
   return (
+    // using router hooks for navigation 
+    
+
     <div>
       <p  className='text-2xl font-bold'>Contact Page</p>
+
+      <div>
+        <button onClick={handleNavigation} type='button' className='btn bg-amber-800 text-white'>Address page</button>
+      </div>
+
     </div>
   )
 }
