7f4bd59c52b85603fee8b2879a0555d95310247c
Iftekar Rahman Ruhit <iftekarrahmanruhit2003@gmail.com>
2025-04-17 23:31:43 +0600

    Dynamic Route and Recap Nested Route

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 
 export default function ServiceDetailsPage({params}) {
+  const data = [
+    {
+      _id: "1",
+      serviceName: "Elite Home Cleaning",
+      serviceImage: "https://via.placeholder.com/150?text=Home+Cleaning",
+      serviceDescription: "Professional and eco-friendly home cleaning services to keep your space spotless."
+    },
+    {
+      _id: "2",
+      serviceName: "TechFix IT Solutions",
+      serviceImage: "https://via.placeholder.com/150?text=IT+Support",
+      serviceDescription: "Expert IT support and troubleshooting for home and office devices."
+    },
+    {
+      _d: "3",
+      serviceName: "FitFlex Personal Training",
+      serviceImage: "https://via.placeholder.com/150?text=Personal+Training",
+      serviceDescription: "One-on-one fitness coaching tailored to your goals, available at home or online."
+    }
+  ]
     const id = params.id;
+    const singleData = data.find(d => d._id == id)
   return (
     <div>
       <h1 className='text-4xl text-white'>Service Details Page</h1>
+      <p className='mt-5'>serviceName:{singleData.serviceName}</p>
+      <p>serviceDescription : {singleData.serviceDescription}</p>
       <p>Id:{id}</p>
     </div>
   )
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,30 +1,41 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
   const data = [
     {
-      "id": "1",
-      "serviceName": "Elite Home Cleaning",
-      "serviceImage": "https://via.placeholder.com/150?text=Home+Cleaning",
-      "serviceDescription": "Professional and eco-friendly home cleaning services to keep your space spotless."
+      _id: "1",
+      serviceName: "Elite Home Cleaning",
+      serviceImage: "https://via.placeholder.com/150?text=Home+Cleaning",
+      serviceDescription: "Professional and eco-friendly home cleaning services to keep your space spotless."
     },
     {
-      "id": "2",
-      "serviceName": "TechFix IT Solutions",
-      "serviceImage": "https://via.placeholder.com/150?text=IT+Support",
-      "serviceDescription": "Expert IT support and troubleshooting for home and office devices."
+      _id: "2",
+      serviceName: "TechFix IT Solutions",
+      serviceImage: "https://via.placeholder.com/150?text=IT+Support",
+      serviceDescription: "Expert IT support and troubleshooting for home and office devices."
     },
     {
-      "id": "3",
-      "serviceName": "FitFlex Personal Training",
-      "serviceImage": "https://via.placeholder.com/150?text=Personal+Training",
-      "serviceDescription": "One-on-one fitness coaching tailored to your goals, available at home or online."
+      _d: "3",
+      serviceName: "FitFlex Personal Training",
+      serviceImage: "https://via.placeholder.com/150?text=Personal+Training",
+      serviceDescription: "One-on-one fitness coaching tailored to your goals, available at home or online."
     }
   ]
   return (
     <div>
       <p  className='text-2xl font-bold'>Services Page</p>
-      
+      {
+        data.map((d)=>{
+          return(
+            <div>
+              <Link href={`services/${d._id}`}>
+              <p className='text-2xl mt-5'>serviceName : {d.serviceName}</p>
+              </Link>
+            </div>
+
+        )})
+      }
     </div>
   )
 }
