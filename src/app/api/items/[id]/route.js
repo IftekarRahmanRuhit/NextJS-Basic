cb2636f8a6ea794a95152fa90ceccb2c22d4e210
Iftekar Rahman Ruhit <iftekarrahmanruhit2003@gmail.com>
2025-07-28 19:20:40 +0600

    added get update delete patch route

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -1,24 +1,27 @@
+import { ObjectId } from 'mongodb';
+import dbConnect from "../../../../lib/dbConnect";
 
-export async function GET(req, params) {
+export async function GET(req, {params}) {
 
     const p = await params
-    console.log(p)
+  const singleData = await dbConnect("products").findOne({ _id: new ObjectId(p.id) });
+  return Response.json(singleData)
+}
+
+export async function DELETE(req, {params}) {
+    
+    const p = await params
+  const deleteData = await dbConnect("products").deleteOne({ _id: new ObjectId(p.id) });
+  return Response.json(deleteData)
  
   return Response.json({params: p })
 }
 
-export async function DELETE(req, params) {
+export async function PATCH(req, {params}) {
     
     const p = await params
-    console.log(p)
- 
-  return Response.json({params: p })
-}
-
-export async function PATCH(req, params) {
-    
-    const p = await params
-    console.log(p)
- 
-  return Response.json({params: p })
+    const postedData = await req.json();
+    const filter = { _id: new ObjectId(p.id) };
+    const updatedData = await dbConnect("products").updateOne(filter,{$set:{...postedData}}, {upsert: true});
+    return Response.json(updatedData)
 }
